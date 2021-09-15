34fc3face79a5ab17226ab63034834aac4b3a38a
Meysam Bavi <meybsky@gmail.com>
2021-09-15 17:37:02 +0430

    Improved example

--- example.py
+++ example.py
@@ -1,19 +1,16 @@
-from digit_set import DigitSetConverter
 from number_to_text import NumberToText
 from time import time
 
-dsc = DigitSetConverter()
-ntt = NumberToText(dsc)
+ntt = NumberToText()
 print(len(ntt))
 
-print('Enter input:')
 try:
     while True:
-        string = input()
+        string = input('Enter input: ')
         start = time().real
-        result = ntt.raw_convert(string)
+        result = ntt.convert(string)
         end = time().real
-        print(f'\t{result} in {(end-start)*1000} ms')
+        print(f'\t{result} ({(end-start)*1000} ms)')
 except EOFError:
     pass
 
